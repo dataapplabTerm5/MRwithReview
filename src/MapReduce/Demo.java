3b757fabec18e9182966dddfb068e37e520b0a01
dataapplabTerm5 <dataapplab@gmail.com>
2016-08-28 19:50:09 -0700

    Update Demo.java

--- src/MapReduce/Demo.java
+++ src/MapReduce/Demo.java
@@ -23,55 +23,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 
  
 public class Demo {
-    /*
-	public static class JsonMapper extends Mapper<Object, Text, Text, InfoWritable> {
-
-		private InfoWritable info = new InfoWritable();
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			try {
-				ObjectMapper mapper = new ObjectMapper();
-	            String[] tuple = value.toString().split("\\n");
-	             for(int i=0;i<tuple.length; i++)
-	             {
-	            	 Business bus = mapper.readValue(tuple[i], Business.class);
-	            	 
-	            	 if(bus.reviewCount != null && Integer.parseInt(bus.reviewCount) > 3)
-	            	 {
-	            		 info.SetName(bus.name);
-	            		 info.SetStars(bus.stars);
-	            		 if(info!= null && bus.city!= null){
-	            		     context.write(new Text(bus.city), info);
-	            		 }
-	            	 }
-	              }
-
-			} catch (JSONException e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-
-        }
-		}
-	}
-	
-	public static class JsonReducer extends Reducer<Text, InfoWritable, Text, Text>{
-		private InfoWritable info = new InfoWritable();
-		private TreeMap<Text,Text> tree = new TreeMap<Text, Text>();
-		public void reduce(Text key, Iterable<InfoWritable> values, Context context) throws IOException, InterruptedException{
-			for(InfoWritable info : values)
-			{
-				tree.put(new Text(info.GetName()),new Text(info.GetStars()));
-				if(tree.size() > 10)
-				{
-					tree.remove(tree.firstKey());
-				}
-			}
-			for(Text t: tree.values())
-			{
-				context.write(key, t);
-			}
-		}
-	}
-	*/
 	
 	public static class JsonMapper extends Mapper<Object, Text, Text, Text> {
 
@@ -125,9 +76,6 @@ public class Demo {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setMapperClass(JsonMapper.class);
-        //job.setReducerClass(JsonReducer.class);
-        //job.setInputFormatClass(KeyValueTextInputFormat.class);
-        //job.setNumReduceTasks(1);
         job.setOutputFormatClass(TextOutputFormat.class);
 
         FileInputFormat.setInputPaths(job, new Path(input));
